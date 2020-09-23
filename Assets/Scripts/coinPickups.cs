28d91d092307f4fb3273a421a92af85b8c48fddf
Ben MacIver <ben@benmaciver.com>
2020-09-23 13:21:57 +0100

    added coinCounter, and started to add coins

---
+++ Assets/Scripts/coinPickups.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinPickups : MonoBehaviour
+{
+    //BoxCollider2D coinCollider;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //coinCollider = GetComponent<BoxCollider2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       bool touchingPlayer = GetComponent<BoxCollider2D>().IsTouching(player.GetComponent<BoxCollider2D>());
+       print(touchingPlayer);
+       if (touchingPlayer == true)
+        {
+            print("coin collected");
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+            
+        }
+
+    }
+
+}
